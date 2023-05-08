568fb3b08ae4c3913b50a0140519b0ba2b18c3c3
Someshwar Wagh <someshwarwagh6165@gmail.com>
2023-05-08 22:11:37 +0530

    AutoIt and SciIt window based popup handle

--- wcsm8seleniumproject/src/MyPracticeAllTopics/TakeScreenShotUsingFileHandler.java
+++ wcsm8seleniumproject/src/MyPracticeAllTopics/TakeScreenShotUsingFileHandler.java
@@ -10,18 +10,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.io.FileHandler;
 
+import com.google.common.io.Files;
+
 public class TakeScreenShotUsingFileHandler {
 	public static void main(String[] args) throws IOException {
 			
 		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-
 		driver.manage().window().maximize();
 		driver.get("https://www.ebay.com/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		//also we can take File Utils to save the screenshot
 		File screenshotFile=((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);	
-		FileHandler.copy(screenshotFile,new File("./screenShots/AccountingLogin.png"));
+		//FileHandler.copy(screenshotFile,new File("./screenShots/AccountingLogin.png"));
+		File dest = new File("./ScreenShots/Xyz.png");
+		Files.copy(screenshotFile, dest);
 		
 		driver.quit();
 		
